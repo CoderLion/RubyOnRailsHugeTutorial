ff22ddd9564f021952018fc735b7ed4608ebc5be
CoderLion <unger.lion@gmail.com>
2017-09-05 15:32:43 +0200

    chapter 8 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'session/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -12,6 +16,12 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
