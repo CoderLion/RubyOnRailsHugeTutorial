d7babcc26d6c5d545c11e46512f7dc8841131974
CoderLion <unger.lion@gmail.com>
2017-09-15 12:43:00 +0200

    chapter 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,20 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      # Fun fact: redirects happen only after a function returns
-      # Subscribe for more fun facts
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        # Fun fact: redirects happen only after a function returns
+        # Subscribe for more fun facts
+        redirect_back_or @user
+      else
+        message = "Account not activated"
+        message += "Check your email for the activation link"
+        flash[:warning] = message
+        # Fun fact: redirects happen only after a function returns
+        # Subscribe for more fun facts
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
+    before_save :downcase_email
+    before_create :create_activation_digest
 
-    before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX },
@@ -25,15 +26,39 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, User.digest(self.remember_token))
     end
 
-    def authenticated?(remember_token)
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
+    end
+
         # remember_digest was created atuomatically with:
         # rails generate migration add_remember_digest_to_users remember_digest:string
         # rails db:migrate
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
+        # return false if remember_digest.nil?
+        # BCrypt::Password.new(remember_digest).is_password?(remember_token)
 
     def forget
         update_attribute(:remember_digest, nil)
     end
+
+    def activate
+        update_columns(activated: true, activated_at: Time.zone.now)
+    end
+
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
+
+
+    private
+
+        def downcase_email
+            self.email = email.downcase
+        end
+
+        def create_activation_digest
+            self.activation_token = User.new_token
+            self.activation_digest = User.digest(activation_token)
+        end
 end
